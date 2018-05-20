e9c34453765046288aaaacf4b2653bbebcaab8db
Gil Tayar <gil@tayar.org>
2018-05-20 10:23:14 +0300

    added header component to storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,6 +3,12 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import StoryRouter from 'storybook-react-router';
 import {Login} from '../components/Login';
+import Header from '../components/Header';
+
+storiesOf('Header', module)
+  .addDecorator(StoryRouter())
+  .add('no user', () => <Header appName="Blog App" />)
+  .add('with user', () => <Header appName="Blog App" currentUser={{userName: 'giltayar'}}/>);
 
 storiesOf('Login', module)
   .addDecorator(StoryRouter())
